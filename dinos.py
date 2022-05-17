2669dfb9f2d50e399904148ed2269f7e7f5e71d2
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-05-16 20:22:49 -0400

    created files and classes based on UML.

---
+++ dinos.py
@@ -0,0 +1,12 @@
+
+from robots import Robot
+robot = Robot
+
+class Dino:
+    def __init__(self, name, attack_power, health):
+        self.name = "T1000"
+        self.attack_power = 10
+        self.health = 100
+
+    def attack(self, robot):
+        pass
---
+++ robots.py
@@ -0,0 +1,14 @@
+
+from weapons import Weapon
+from dinos import Dino
+
+dinosaur = Dino
+
+class Robot:
+    def __init__(self, name, health, active_weapon):
+        self.name = "T1000"
+        self.health = 100
+        self.active_weapon = Weapon
+
+    def attack(self, dinosaur):
+        pass
