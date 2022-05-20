3fdfa1d9d6d51b18aa236b1288c6448e89762818
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-05-20 00:21:36 -0400

    Added 1 second sleep between print lines in BATTLEFIELD.PY

--- battlefield.py
+++ battlefield.py
@@ -1,6 +1,7 @@
 
 from dinos import Dino
 from robots import Robot
+from time import sleep
 
 robot_1 = Robot()
 dino_1 = Dino()
@@ -24,25 +25,37 @@ class Battlefield:
         
     def display_welcome(self):
         print("WELCOME TO ROBOT VS DINOSAUR!")
+        sleep(1.5)
+        print(f"{dino_1.name} attack power is {dino_1.attack_power}!")
+        sleep(1.5)
+        print(f"{robot_1.name} attack power is {robot_1.attack_power}!")
+        sleep(1.5)
 
     def battle_phase(self):
         while dino_1.health and robot_1.health >= 0:
             robot_1.attack(dino_1)
             dino_1.health = dino_1.health - robot_attack
             print(f"{robot_1.name} has attacked {dino_1.name} with a {robot_1.weapon}!")
+            sleep(1)
             print(f"{dino_1.name}'s current health is {dino_1.health}!")
+            sleep(1)
             robot_1.health = robot_1.health - dino_attack
             dino_1.attack(robot_1)
             print(f"{dino_1.name} has attacked {robot_1.name}!")
+            sleep(1)
             print(f"{robot_1.name}'s current health is {robot_1.health}!")
+            sleep(1)
         if dino_1.health <= 0:
             print(f"{dino_1.name} is EXTINCT! {robot_1.name} is unstoppable!")
+            sleep(1)
         elif robot_1.health <= 0:
             print(f"{robot_1.name} has been terminated!")
+            sleep(1)
             
     def display_winner(self):
         if robot_1.health <= dino_1.health:
             print(f"{dino_1.name} rules all!")
+            sleep(1)
         elif dino_1.health <= robot_1.health:
             print(f"{robot_1.name} wins!")
         
